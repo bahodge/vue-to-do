412d516249157b32c9af34cfb899ffbfb4b2a4b7
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2019-06-27 10:21:35 -0500

    working

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Hey</div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem"
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <p v-for="todo in todo" v-bind:key="todo.id">
+      <TodoItem/>
+    </p>
+  </div>
+</template>
+
+
+<script>
+import TodoItem from "./TodoItem";
+export default {
+  name: "Todo",
+  data() {
+    return {
+      todos: [
+        { title: "Do Work", id: 1, completed: true },
+        { title: "Do More Work", id: 2, completed: false },
+        { title: "Do Even more work!", id: 3, completed: false }
+      ]
+    };
+  }
+};
+</script>
+
+
